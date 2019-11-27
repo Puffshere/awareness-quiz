f7186504e9d3103e72dcc14fd247cef09d3c7475
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-11-26 17:00:22 -0800

    adds hamburger button

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("panel").style.display = "block";
+}
+
+function myFunctionClose() {
+    document.getElementById("panel").style.display = "none";
+}
+
